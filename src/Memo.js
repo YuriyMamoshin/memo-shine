0c3bd522b5e0dfe8e54b9974b72b8ec4a405dd64
YuriyMamoshin <122636215+YuriyMamoshin@users.noreply.github.com>
2023-02-14 21:36:10 +0200

    Added some basic CSS

--- src/Memo.js
+++ src/Memo.js
@@ -1,8 +1,8 @@
 export default function Memo(props) {
     return (
         <div className="memo-container">
-            <div>{props.answer}</div>
-            <div>{props.definition}</div>
+            <div className="memo-answer">{props.answer}</div>
+            <div className="memo-definition">{props.definition}</div>
         </div>
     )
 }
--- src/Start.js
+++ src/Start.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Form from "./Form";
 import Memo from "./Memo";
+import { nanoid } from "nanoid";
+
 export default function Start() {
 
     const [data, setData] = useState([]);
@@ -18,6 +20,7 @@ export default function Start() {
         setData(oldData => {
             const dataArray = oldData[0].split(/\r?\n/);
             return dataArray.map(piece => ({
+                id: nanoid(),
                 answer: piece.split(" - ")[0],
                 definition: piece.split(" - ")[1],
             }))
@@ -28,6 +31,7 @@ export default function Start() {
 
     const memos = data.map(memo => {
         return  <Memo
+        key={memo.id}
             answer={memo.answer}
             definition={memo.definition}
         />
