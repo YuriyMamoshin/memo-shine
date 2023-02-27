bc63af5d879a61b93d9bb1c08e56ad8a4d5019ad
YuriyMamoshin <122636215+YuriyMamoshin@users.noreply.github.com>
2023-02-27 21:11:33 +0200

    Changed some styling

--- src/Memo.js
+++ src/Memo.js
@@ -8,7 +8,7 @@ export default function Memo(props) {
 
             <Grade
                 grade={props.grade}
-                class="grade-button correct-button"
+                class="grade-button correct-button memo-button"
                 style={{ visibility: props.checked ? "visible" : "hidden" }}
                 content="Correct"
                 memoId={props.memoId}
@@ -16,18 +16,18 @@ export default function Memo(props) {
             />
             <Grade
                 grade={props.grade}
-                class="grade-button incorrect-button"
+                class="grade-button incorrect-button memo-button"
                 style={{ visibility: props.checked ? "visible" : "hidden" }}
                 content="Incorrect"
                 memoId={props.memoId}
                 id={0}
             />
 
-            <div className="memo-answer" onClick={props.check}>{
+            <div className="memo-answer memo-button" onClick={props.check}>{
                 props.checked ? props.answer : "???"
             }
             </div>
-            <div className="memo-definition">{props.definition}</div>
+            <div className="memo-definition memo-button">{props.definition}</div>
         </div>
     )
 }
--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,15 @@
 export default function Result(props) {
-const correctArray = props.correct.map(obj => obj.answer);
-const correctString = correctArray.join(", ");
+    const correctArray = props.correct.map(obj => obj.answer);
+    const correctString = correctArray.join(", ");
 
     return (
-        <div>
-            You have answered these questions correctly, so you might delete them from database as fully learned:
-           <p>{correctString}</p> 
-            </div>
+        <div className="result-container">
+            {correctString ? <p> You have answered these questions correctly, so you might delete them from database as fully learned:
+                <p> {correctString} </p> </p> :
+                <p>You have no correct answers</p>
+                }
+                
+            <button onClick={props.next}>Next stage</button>
+        </div>
     )
 }
